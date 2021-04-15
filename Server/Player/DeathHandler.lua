68b0474c4d23000b7f8d38ee61256bc77df59a86
EinfachMaxYT <72171280+EinfachMaxYT@users.noreply.github.com>
2021-04-15 10:41:54 +0200

    Set Spectating the killer

--- Server/Player/DeathHandler.lua
+++ Server/Player/DeathHandler.lua
@@ -8,6 +8,10 @@ Character:Subscribe("Death", function(character)
 	player:SetVOIPMuted(true)	
 	player:SetAlive(false)
 	
+	if(player:GetData("TTT_LastDamager") not nil and player:GetData("TTT_LastDamager") not player) then
+		player:SetSpectating(player:GetData("TTT_LastDamager"))
+	end
+	
 	if(TTT.match_state == MATCH_STATES.IN_PROGRESS) then
 
 		-- INNO WIN
@@ -61,13 +65,13 @@ Character:Subscribe("Death", function(character)
 		if(player:GetRole() ~= ROLES.TRAITOR) then
 
 			local lastDamagePlayer = player:GetData("TTT_LastDamager")
-            if(lastDamagePlayer == nil or lastDamagePlayer == player) then return end			
+            		if(lastDamagePlayer == nil or lastDamagePlayer == player) then return end			
 			
 			player:SetSpectating(lastDamagePlayer)
 
 			if(lastDamagePlayer:GetRole() ~= ROLES.INNOCENT) then return end
 			lastDamagePlayer:RemoveKarma(50) -- Innocent der diesen Spieler getötet hat, verliert 50 Karma
-            lastDamagePlayer:SendNotification("You lost 50 Karma because you killed a innocent")
+           		lastDamagePlayer:SendNotification("You lost 50 Karma because you killed a innocent")
 		end		
 	end
 end)
